7012d30f20a11052c63d9d1e22f93501a886d7a3
Chris Gillis <771603+lunchbox929@users.noreply.github.com>
2023-04-03 09:54:16 -0400

    NvChadified

--- nvim/init.lua
+++ nvim/init.lua
@@ -1,8 +1,21 @@
-require('settings')
-require('packer_setup')
-require('load_plugins')
-require('keymaps')
-require('lsp_setup')
-require('load_plugins')
--- The line beneath this is called `modeline`. See `:help modeline`
--- vim: ts=2 sts=2 sw=2 et
+require "core"
+
+local custom_init_path = vim.api.nvim_get_runtime_file("lua/custom/init.lua", false)[1]
+
+if custom_init_path then
+  dofile(custom_init_path)
+end
+
+require("core.utils").load_mappings()
+
+local lazypath = vim.fn.stdpath "data" .. "/lazy/lazy.nvim"
+
+-- bootstrap lazy.nvim!
+if not vim.loop.fs_stat(lazypath) then
+  require("core.bootstrap").gen_chadrc_template()
+  require("core.bootstrap").lazy(lazypath)
+end
+
+dofile(vim.g.base46_cache .. "defaults")
+vim.opt.rtp:prepend(lazypath)
+require "plugins"
