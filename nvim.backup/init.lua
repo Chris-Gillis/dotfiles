93d2b8a106daf7ac0f887915c45476247a68cc81
Chris Gillis <771603+lunchbox929@users.noreply.github.com>
2023-04-04 14:51:33 -0400

    whatever

---
+++ nvim.backup/init.lua
@@ -0,0 +1,8 @@
+require('settings')
+require('packer_setup')
+require('load_plugins')
+require('keymaps')
+require('lsp_setup')
+require('load_plugins')
+-- The line beneath this is called `modeline`. See `:help modeline`
+-- vim: ts=2 sts=2 sw=2 et
--- nvim.backup/lua/packer_setup.lua
+++ nvim.backup/lua/packer_setup.lua
@@ -50,10 +50,12 @@ require('packer').startup(function(use)
   use 'nvim-lualine/lualine.nvim' -- Fancier statusline
   use 'lukas-reineke/indent-blankline.nvim' -- Add indentation guides even on blank lines
   use 'tpope/vim-sleuth' -- Detect tabstop and shiftwidth automatically
+  use 'tpope/vim-dispatch' -- Asynchronous build and test dispatcher
 
   -- Rails plugins
   use 'tpope/vim-rails'
-  use 'tpop/vim-bundler'
+
+  use 'github/copilot.vim'
 
   use {
     'numToStr/Comment.nvim', -- "gc" to comment visual regions/lines
@@ -78,6 +80,7 @@ require('packer').startup(function(use)
   use 'fatih/vim-go'
   use 'simrat39/rust-tools.nvim'
 
+  use 'folke/neodev.nvim'
   -- Add custom plugins to packer from ~/.config/nvim/lua/custom/plugins.lua
   local has_plugins, plugins = pcall(require, 'custom.plugins')
   if has_plugins then
